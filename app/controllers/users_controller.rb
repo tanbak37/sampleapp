b26f32baeace11605879d20db07a948233359349
Anjenk <jimmy.dbajo@gmail.com>
2015-08-04 07:19:00 +0000

    memperbaiki link email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-        @microposts = @user.microposts.paginate(page: params[:page])
-
+    @microposts = @user.microposts.paginate(page: params[:page])
+    
   end
 
   def new
