a679009ffd743af8e6eac6a2a4ed422f9135bf9c
Anjenk <jimmy.dbajo@gmail.com>
2016-04-10 13:57:14 +0000

    buat tugas rpl

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -6,7 +6,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "Postingan berhasil dibuat!"
       redirect_to root_url
     else
       @feed_items = []
@@ -16,7 +16,7 @@ class MicropostsController < ApplicationController
 
    def destroy
     @micropost.destroy
-    flash[:success] = "Micropost deleted"
+    flash[:success] = "Postingan berhasil dihapus"
     redirect_to request.referrer || root_url
    end
 
